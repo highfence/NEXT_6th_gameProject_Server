4e179a5a06128de94115338cbb5fd6e75ec34e90
Lee Kwenwon <highfence@naver.com>
2017-11-05 17:44:11 +0900

    [ADD] Initialize LoginServer & git ignore for Visual Studio

---
+++ LoginServer/NextManComing_LoginServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NextManComing_LoginServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
