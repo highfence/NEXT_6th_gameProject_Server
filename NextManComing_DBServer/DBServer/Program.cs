7bb8fc68874b686d29ccf38f95816e4ba02706f8
Lee Kwenwon <highfence@naver.com>
2017-11-05 17:45:43 +0900

    [ADD] Initialize DBServer Solution

---
+++ NextManComing_DBServer/DBServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DBServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
