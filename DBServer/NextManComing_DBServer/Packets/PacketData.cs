06c9bc6cddde7e8846fddd534718a0fb73f2fc5f
Lee Kwenwon <highfence@naver.com>
2017-11-05 19:34:59 +0900

    [ADD] 로그인 시도에 쓸 패킷 데이터만 만들어 놓음

---
+++ DBServer/NextManComing_DBServer/Packets/PacketData.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NextManComing_DBServer
+{
+	// 로그인 서버와 통신하기 위해 정의된 패킷 구조체들.
+	internal static class LoginServerPacket
+	{
+		public struct LoginReq
+		{
+			public string UserId;
+			public string UserPw;
+		}
+
+		public struct LoginRes
+		{
+			public short Result;
+			public Int64 Token;
+		}
+	}
+
+	// 게임 서버와 통신하기 위해 정의된 패킷 구조체들.
+	internal static class GameServerPacket
+	{
+
+	}
+}
