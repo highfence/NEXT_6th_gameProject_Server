0076f5ff7f5e7f665927cc7dee3f50b964edc917
Lee Kwenwon <highfence@naver.com>
2017-11-06 03:01:42 +0900

    [ERROR] Owin이 호환될 줄 알았는데 .NET CORE라서 호환이 안되는듯. 실행이 안됨

--- DBServer/NextManComing_DBServer/Program Main/Program.cs
+++ DBServer/NextManComing_DBServer/Program Main/Program.cs
@@ -13,19 +13,19 @@ namespace NextManComing_DBServer
 			var addressBuilder = new StringBuilder(20);
 			addressBuilder.AppendFormat("http://*:{0}/", config.DBServerPort);
 
-			using (WebApp.Start<Startup>(url : addressBuilder.ToString()))
+			using (WebApp.Start<Startup>(url: addressBuilder.ToString()))
 			{
 				var result = DBServerMain.Init();
 
 				if (result != ErrorCode.None)
 				{
 					Console.WriteLine("DBServer Initialize Failed. ErrorCode : ", result);
-					return; 
+					return;
 				}
-			}
 
-			Console.WriteLine("DBServer Initialized. BaseAddress : ", addressBuilder.ToString());
-			Console.ReadLine();
+				Console.WriteLine("DBServer Initialized. BaseAddress : ", addressBuilder.ToString());
+				Console.ReadLine();
+			}
         }
     }
 }
--- LoginServer/NextManComing_LoginServer/Program Main/Program.cs
+++ LoginServer/NextManComing_LoginServer/Program Main/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Owin.Hosting;
+using System;
+using System.Text;
 
 namespace NextManComing_LoginServer
 {
@@ -8,8 +10,17 @@ namespace NextManComing_LoginServer
         {
 			var config = LoginServerConfig.GetInstance();
 
+			var addressBuilder = new StringBuilder(20);
+			addressBuilder.AppendFormat("http://*:{0}/", config.LoginServerPort);
+			//var baseAddress = addressBuilder.ToString();
+			var baseAddress = "http://localhost:18000/";
 
-
+			using (WebApp.Start<Startup>(url : baseAddress))
+			{
+				Console.WriteLine("LoginServer Initialized. BaseAddress : " + baseAddress);
+				Console.WriteLine("LoginServer Listening... IPv4 Address is " + Util.GetLocalIpAddress());
+				Console.ReadLine();
+			}
         }
     }
 }
