ade042a3a984c0f68f980b9268a404685fa4d358
Lee Kwenwon <highfence@naver.com>
2017-11-05 19:06:01 +0900

    [ADD] 데이터 보관소 싱글톤

---
+++ LoginServer/NextManComing_LoginServer/Utils/DataContainer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NextManComing_LoginServer
+{
+    internal class DataContainer
+    {
+		private static DataContainer instance;
+
+		protected DataContainer() { }
+
+		public static DataContainer GetInstance()
+		{
+			if (instance == null)
+			{
+				instance = new DataContainer();
+			}
+
+			return instance;
+		}
+    }
+}
