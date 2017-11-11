0d0e6c6d1c6a29366ffc475ac124d3f49ccdfb59
Lee Kwenwon <highfence@naver.com>
2017-11-11 18:05:35 +0900

    [DELETE] DBServer .NET CORE Solution Delete

--- LoginServer/NextManComing_LoginServer/Program Main/Program.cs
+++ LoginServer/NextManComing_LoginServer/Program Main/Program.cs
@@ -4,9 +4,10 @@ namespace NextManComing_LoginServer
 {
     class Program
     {
+		static string baseAddress = "http://localhost:18000/";
+
         static void Main(string[] args)
         {
-
         }
     }
 }
