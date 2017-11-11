d7a891c119436b7e633be715c89f389e60f908ae
Lee Kwenwon <highfence@naver.com>
2017-11-11 18:55:22 +0900

    [ADD] Port To ASPServer Using Kestrel

---
+++ LoginServer/NextManComing_LoginServer/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace NextManComing_LoginServer
+{
+    public class Program
+    {
+		static string baseAddress = "http://localhost:18000/";
+
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+		public static IWebHost BuildWebHost(string[] args) =>
+
+			WebHost.CreateDefaultBuilder(args)
+				.UseKestrel()
+				.UseStartup<Startup>()
+				.UseUrls(baseAddress)
+                .Build();
+    }
+}
