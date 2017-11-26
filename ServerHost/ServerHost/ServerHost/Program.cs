5b6ade5dc1af386254573eec4838d58f92f3d484
DESKTOP-I3BIINJ\LittleHill <massivedynamicplay@gmail.com>
2017-11-26 23:26:03 +0900

    [ADD]Nlog 사용 테스트 클래스와 nlog.config 파일 추가 서버 호스트의 nlog.config 파일을 다른 참조 라이브러이 에서 사용 가능 하므로 각각의 라이브러리 마다. Install-Package NLog.Extensions.Logging -Version 1.0.0-rtm-rc2 명령어로 패키지 매니져 에서 패키지를 추가한후 로거를 사용하면 된다.

--- ServerHost/ServerHost/ServerHost/Program.cs
+++ ServerHost/ServerHost/ServerHost/Program.cs
@@ -1,12 +1,23 @@
 ﻿using System;
-
+using NLog;
 namespace ServerHost
 {
     class Program
     {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            logger.Trace("test Trace");
+
+            logger.Debug("test Debug");
+
+            logger.Info("test Info");
+
+            logger.Error("test Error");
+
+            logger.Fatal("test Fatal");
         }
     }
 }
