5ca3850faf5a5cea0d513e1767d6524b3636f065
DESKTOP-I3BIINJ\LittleHill <massivedynamicplay@gmail.com>
2017-12-04 14:54:11 +0900

    [ADD]UserManager class,ErrorCode for Logic,Vector3 class and User class  Added. it need to be merge with Library/MapLibrary branch

---
+++ Library/LogicLibrary/Vector3.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogicLibrary
+{
+    class Vector3
+    {
+        public float x { get; set; }
+        public float y { get; set; }
+        public float z { get; set; }
+
+        public Vector3(float xVal, float yVal, float zVal)
+        {
+            x = xVal;
+
+            y = yVal;
+
+            z = zVal;
+        }
+
+        public static Vector3 operator +(Vector3 c1, Vector3 c2)
+        {
+            return new Vector3(c1.x + c2.x, c1.y + c2.y, c1.z + c2.z);
+        }
+    }
+}
--- Library/NetworkLibrary/NetworkLibrary/IUserManager.cs
+++ Library/NetworkLibrary/NetworkLibrary/IUserManager.cs
@@ -8,6 +8,14 @@ namespace NetworkLibrary
 	public interface IUserManager
 	{
 		// 해당 세션에 연결된 유저가 유효한지 확인해주는 메소드.
-		bool IsUserExist(ClientSession owner);
-	}
+        bool IsUserExist(ClientSession owner);
+
+        int AllocNewUserToSession(ClientSession newSession, string userID);
+
+        int FreeUser(ClientSession targetSession);
+
+        bool IsFull();
+
+        bool IsEmpty();
+    }
 }
